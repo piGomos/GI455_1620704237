63d4ae0ba82dcbd8451252d3a7620c511a72a538
DESKTOP-7KB1O16\nuchp <piyanuch.prom@bumail.net>
2021-01-29 00:36:57 +0700

    Send Assignment Week1

---
+++ GI455_Project/Assets/Seacrhbar.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Seacrhbar : MonoBehaviour
+{
+    public InputField game;
+    public Text textBox;
+
+    public void SeacrhGame()
+    {
+        var gameName = game.text;
+        
+        switch (gameName)
+        {
+            case "Overwatch":
+                textBox.text = game.text + " is found";
+                break;
+            case "Maplestory":
+                textBox.text = game.text + " is found";
+                break;
+            case "Thesims":
+                textBox.text = game.text + " is found";
+                break;
+            case "Pangya":
+                textBox.text = game.text + " is found";
+                break;
+            case "Minecraft":
+                textBox.text = game.text + " is found";
+                break;
+            default:
+                textBox.text = game.text + " is not found";
+                break;
+        }
+        
+    }
+    
+}
